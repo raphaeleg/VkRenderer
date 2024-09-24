d9a5cd63be5f8cc8b200d67f769640d4669b0e48
raphaeleg <raphaeleguillemotmichelle@gmail.com>
2024-09-24 10:20:54 +0800

    Colorful Triangle Flower

--- Source/first_app.cpp
+++ Source/first_app.cpp
@@ -164,12 +164,17 @@ namespace lve{
     }
 
     void FirstApp::RenderGameObjects(VkCommandBuffer commandBuffer) {
+        // update
+        int i = 0;
+        for (auto& obj : gameObjects) {
+            i += 1;
+            obj.transform2d.rotation =
+                glm::mod<float>(obj.transform2d.rotation + 0.0005f * i, 2.f * glm::pi<float>());
+        }
+
+        // render
         lvePipeline->Bind(commandBuffer);
         for (auto& obj : gameObjects) {
-            obj.transform2d.rotation = glm::mod(
-                obj.transform2d.rotation + 0.01f,
-                glm::two_pi<float>());
-
             SimplePushConstantData push{};
             push.offset = obj.transform2d.translation;
             push.color = obj.color;
@@ -182,7 +187,6 @@ namespace lve{
                 0,
                 sizeof(SimplePushConstantData),
                 &push);
-
             obj.model->Bind(commandBuffer);
             obj.model->Draw(commandBuffer);
         }
@@ -213,20 +217,30 @@ namespace lve{
 
     void FirstApp::LoadGameObjects() {
         std::vector<LveModel::Vertex> vertices{
-          {{  0.0f, -0.5f }, { 1.0f, 0.0f, 0.0f }},
-          {{  0.5f,  0.5f }, { 0.0f, 1.0f, 0.0f }},
-          {{ -0.5f,  0.5f }, { 0.0f, 0.0f, 1.0f }}
-        };
+      {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+      {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+      {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}} };
         auto lveModel = std::make_shared<LveModel>(lveDevice, vertices);
 
-        auto triangle = LveGameObject::CreateGameObject();
-        triangle.model = lveModel;
-        triangle.color = { 0.1f, 0.8f, 0.1f };
-        triangle.transform2d.translation.x = 0.2f;
-        triangle.transform2d.scale = {2.0f, 0.5f };
-        triangle.transform2d.rotation = 0.25f * glm::two_pi<float>();
-
-        gameObjects.push_back(std::move(triangle));
+        // https://www.color-hex.com/color-palette/5361
+        std::vector<glm::vec3> colors{
+            {1.f, .7f, .73f},
+            {1.f, .87f, .73f},
+            {1.f, 1.f, .73f},
+            {.73f, 1.f, .8f},
+            {.73, .88f, 1.f}  //
+        };
+        for (auto& color : colors) {
+            color = glm::pow(color, glm::vec3{ 2.2f });
+        }
+        for (int i = 0; i < 40; i++) {
+            auto triangle = LveGameObject::CreateGameObject();
+            triangle.model = lveModel;
+            triangle.transform2d.scale = glm::vec2(.5f) + i * 0.025f;
+            triangle.transform2d.rotation = i * glm::pi<float>() * .025f;
+            triangle.color = colors[i % colors.size()];
+            gameObjects.push_back(std::move(triangle));
+        }
     }
 
 }	// namespace lve
